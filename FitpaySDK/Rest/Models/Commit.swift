fed08f3728e1d36891baeca8fbdd0bfc5f7d171b
Igor Kravchenko <igman2005@gmail.com>
2016-02-19 14:52:08 +0200

    Removes all fields from Payload class; adds 'info' field of type Dictionary to make payload object more generic

--- FitpaySDK/Rest/Models/Commit.swift
+++ FitpaySDK/Rest/Models/Commit.swift
@@ -21,24 +21,5 @@ public enum CommitType : String
 
 public class Payload
 {
-    var createdEpoch:Int?
-    var reason:String?
-    var cvv:String?
-    var address:Address?
-    var externalTokenReference:String?
-    var deviceRelationships:[DeviceRelationships]?
-    var cardType:String?
-    var causedBy:CreditCardInitiator?
-    var lastModifiedEpoch:Int?
-    var userId:String?
-    var created:String?
-    var lastModified:String?
-    var expMonth:Int?
-    var targetDeviceType:String?
-    var expYear:Int?
-    var targetDeviceId:String?
-    var name:String?
-    var state:String? //TODO: consider adding enum
-    var pan:String?
-    var cardMetaData:CardMetadata?
+    var info = [String : AnyObject]()
 }
