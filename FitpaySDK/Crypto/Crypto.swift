0208c32849af4a127a99b07dd90182b37d652fc3
walkline <walkline.ua@gmail.com>
2016-02-26 16:03:00 +0200

    Implemented generation of shared secret for key pairs.

--- FitpaySDK/Crypto/Crypto.swift
+++ FitpaySDK/Crypto/Crypto.swift
@@ -27,6 +27,27 @@ class Crypto
         return key
     }
     
+    func generateSecretForPublicKey(publicKey: String!) -> NSData?
+    {
+        guard let cPublicKey = publicKey.cStringUsingEncoding(NSUTF8StringEncoding) else {
+            return nil
+        }
+        
+        guard cPublicKey.count > 1 else {
+            return nil
+        }
+        
+        let secretResult = UnsafeMutablePointer<SECP256R1_SharedSecret>.alloc(sizeof(SECP256R1_SharedSecret))
+        
+        secp256r1_generate_secret(keyPair, strdup(cPublicKey), secretResult)
+        
+        let data = NSData(bytes: &secretResult.memory.secret, length: Int(secretResult.memory.secret_size))
+        
+        secretResult.dealloc(sizeof(SECP256R1_SharedSecret))
+        
+        return data
+    }
+    
     init()
     {
         SECP256R1_GenerateKeyPair(keyPair);
--- FitpaySDK/FPCrypto/SECP256R1.h
+++ FitpaySDK/FPCrypto/SECP256R1.h
@@ -85,7 +85,7 @@ static void SECP256R1_GenerateKeyPair(SECP256R1_KeyPair *key_pair)
         char * strX = BN_bn2hex(bigNumX);
         char * strY = BN_bn2hex(bigNumY);
 
-        if((client_public_key = malloc(strlen(ans1PubKeyEncoding) + strlen(strX) + strlen(strY) + 1)) != NULL)
+        if ((client_public_key = malloc(strlen(ans1PubKeyEncoding) + strlen(strX) + strlen(strY) + 1)) != NULL)
         {
             client_public_key[0] = '\0';
             strcat(client_public_key, ans1PubKeyEncoding);
@@ -113,4 +113,117 @@ static void SECP256R1_GenerateKeyPair(SECP256R1_KeyPair *key_pair)
     BN_free(client_publicK_y);
     BN_free((BIGNUM *)client_privateKey);
     BN_CTX_free(bn_ctx);
+}
+
+struct SECP256R1_SharedSecret {
+    unsigned char secret[256];
+    int secret_size;
+};
+
+typedef struct SECP256R1_SharedSecret SECP256R1_SharedSecret;
+
+static void secp256r1_generate_secret(SECP256R1_KeyPair * key_pair, char * public_key, SECP256R1_SharedSecret * secret)
+{
+    static char ans1PubKeyEncoding[] = "3059301306072a8648ce3d020106082a8648ce3d03010703420004\0";
+    
+    unsigned long keySize = strlen(public_key);
+    unsigned long ans1Size = strlen(ans1PubKeyEncoding);
+    unsigned long bigNumSize = (keySize - ans1Size) / 2;
+    
+    char * xHex = malloc((bigNumSize + 1) * sizeof(char));
+    memcpy(xHex, public_key+ans1Size, bigNumSize);
+    xHex[bigNumSize] = '\0';
+    
+    char * yHex = malloc((bigNumSize + 1) * sizeof(char));
+    memcpy(yHex, public_key + ans1Size + bigNumSize, bigNumSize);
+    yHex[bigNumSize] = '\0';
+    
+    BN_CTX* bn_ctx;
+    
+    EC_KEY*   server_key_curve = NULL;
+    EC_GROUP* server_key_group = NULL;
+    EC_POINT* server_publicKey = NULL;
+    
+    BIGNUM* server_publicK_x = NULL;
+    BIGNUM* server_publicK_y = NULL;
+    
+    bn_ctx = BN_CTX_new();
+    BN_CTX_start(bn_ctx);
+    
+    server_publicK_x = BN_CTX_get(bn_ctx);
+    server_publicK_y = BN_CTX_get(bn_ctx);
+    
+    BN_hex2bn(&server_publicK_x, xHex);
+    BN_hex2bn(&server_publicK_y, yHex);
+    
+    free(xHex);
+    free(yHex);
+    
+    if ((server_key_curve = EC_KEY_new_by_curve_name(NID_X9_62_prime256v1)) == NULL)
+    {
+        BN_CTX_free(bn_ctx);
+        return;
+    }
+    
+    if ((server_key_group = (EC_GROUP *)EC_KEY_get0_group(server_key_curve)) == NULL)
+    {
+        BN_CTX_free(bn_ctx);
+        EC_KEY_free(server_key_curve);
+        return;
+    }
+    
+    if (EC_KEY_generate_key(server_key_curve) != 1)
+    {
+        BN_CTX_free(bn_ctx);
+        EC_KEY_free(server_key_curve);
+        return;
+    }
+    
+    if ((server_publicKey = EC_POINT_new(server_key_group)) == NULL)
+    {
+        BN_CTX_free(bn_ctx);
+        EC_KEY_free(server_key_curve);
+        return;
+    }
+    
+    if (EC_POINT_set_affine_coordinates_GFp(server_key_group, server_publicKey, server_publicK_x, server_publicK_y, bn_ctx) != 1)
+    {
+        EC_POINT_free(server_publicKey);
+        BN_CTX_free(bn_ctx);
+        EC_KEY_free(server_key_curve);
+        return;
+    }
+    
+    if (EC_KEY_check_key(server_key_curve) != 1)
+    {
+        EC_POINT_free(server_publicKey);
+        BN_CTX_free(bn_ctx);
+        EC_KEY_free(server_key_curve);
+        return;
+    }
+    
+    int field_size = EC_GROUP_get_degree(server_key_group);
+    size_t len = (field_size + 7) / 8;
+    unsigned char *key_agreement = NULL;
+    key_agreement = (unsigned char *)OPENSSL_malloc(len);
+    
+    if (ECDH_compute_key(key_agreement, len, server_publicKey, key_pair->key, 0) == 0)
+    {
+        EC_POINT_free(server_publicKey);
+        BN_CTX_free(bn_ctx);
+        EC_KEY_free(server_key_curve);
+        free(key_agreement);
+        return;
+    }
+    
+    memcpy(secret->secret, key_agreement, len);
+    secret->secret_size = (int)len;
+    
+    EC_POINT_free(server_publicKey);
+    EC_KEY_free(server_key_curve);
+    BN_CTX_free(bn_ctx);
+    
+    free(key_agreement);
+    
+    return secret;
 }
