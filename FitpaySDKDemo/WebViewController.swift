20ed200e815a373698e756625b7fc037f5fa9cc8
Benjamin Walford <ben@fit-pay.com>
2016-05-13 14:56:09 -0600

    change RtmNative class name to FPWebView

--- FitpaySDKDemo/WebViewController.swift
+++ FitpaySDKDemo/WebViewController.swift
@@ -7,21 +7,21 @@ import ObjectMapper
 class WebViewController: UIViewController {
     @IBOutlet var containerView : UIView! = nil
     var webView = WKWebView()
-    var rtm: RtmNative?
+    var fp: FPWebView?
     
     override func viewDidLoad() {
         let device = PaymentDevice();
         device.changeDeviceInterface(MockPaymentDeviceInterface(paymentDevice: device))
-        
-        rtm = RtmNative(clientId: "pagare", redirectUri: "http://example.com", paymentDevice: device)
-        let config:WKWebViewConfiguration = rtm!.wvConfig()
+
+        fp = FPWebView(clientId: "pagare", redirectUri: "http://example.com", paymentDevice: device)
+        let config:WKWebViewConfiguration = fp!.wvConfig()
         
         self.view.frame = self.view.bounds
         self.webView = WKWebView(frame: self.view.frame, configuration: config)
         
         self.view = self.webView
-        self.webView.loadRequest((rtm!.wvRequest()))
-        rtm?.setWebView(webView)
+        self.webView.loadRequest((fp!.wvRequest()))
+        fp?.setWebView(webView)
 
         bindToEvents()
     }
