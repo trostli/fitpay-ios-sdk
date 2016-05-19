39d1cba5a5b8a1f08bd8513da9854bd4288906e5
Carol <carol@fit-pay.com>
2016-05-19 09:27:42 -0600

    revert constants to demo

--- Common/Constants/RestAPIConstants.swift
+++ Common/Constants/RestAPIConstants.swift
@@ -1,3 +1,4 @@
-internal let BASE_URL = "http://192.168.136.76:8001"
+
+internal let BASE_URL = "https://demo.pagare.me"
 internal let API_BASE_URL = "\(BASE_URL)/api"
 internal let AUTHORIZE_URL = "\(BASE_URL)/oauth/authorize"
